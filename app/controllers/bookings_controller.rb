55e8814f170ca3d562c30c4290edd6989cedcd23
Jephte Extra <jephteextra@gmail.com>
2023-09-20 22:04:32 -0400

    Add booking new page and new action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,23 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: %i[show]
   def index
     @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find_by(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start, :end, :purpose, :room_id)
   end
 end
